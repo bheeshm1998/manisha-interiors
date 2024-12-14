0ce4c4d6bbd9befc7afe1d39f43fcf367f77e869
PhysicsIsLove <aa.iitbbs@gmail.com>
2024-12-15 02:30:33 +0530

    added carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,30 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  featuredProjects = [
-    {
-      title: 'Modern Living Room',
-      description: 'Contemporary design with comfort in mind',
-      image: 'assets/images/portfolio/living-room.jpg'
-    },
-    {
-      title: 'Luxury Kitchen',
-      description: 'Elegant and functional kitchen design',
-      image: 'assets/images/portfolio/kitchen.jpg'
-    },
-    {
-      title: 'Peaceful Bedroom',
-      description: 'Serene bedroom retreat',
-      image: 'assets/images/portfolio/bedroom.jpg'
+export class HomeComponent implements OnInit {
+  images: string[] = [];
+  currentIndex = 0;
+  private autoSlideInterval: any;
+
+  ngOnInit() {
+    this.images = [
+      'assets/images/gallery/image (1).jpg',
+      'assets/images/gallery/image (2).jpg',
+      'assets/images/gallery/image (3).jpg',
+      'assets/images/gallery/image (4).jpg',
+      'assets/images/gallery/image (5).jpg',
+      'assets/images/gallery/image (6).jpg',
+    ];
+    
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  previousSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  private startAutoSlide() {
+    this.autoSlideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Change slide every 5 seconds
+  }
+
+  private stopAutoSlide() {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
     }
-  ];
+  }
 }
