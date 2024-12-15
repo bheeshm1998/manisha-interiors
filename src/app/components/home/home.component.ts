1890cdb61a1769147e7cde26362466c38381f374
PhysicsIsLove <aa.iitbbs@gmail.com>
2024-12-15 14:52:36 +0530

    fixed a foreever reloading bug

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';  // Import ChangeDetectorRef
 
 @Component({
   selector: 'app-home',
@@ -8,10 +10,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   images: string[] = [];
   currentIndex = 0;
   private autoSlideInterval: any;
+
+  constructor(private cdRef: ChangeDetectorRef) {}  // Inject ChangeDetectorRef
 
   ngOnInit() {
     this.images = [
@@ -23,28 +27,32 @@ export class HomeComponent implements OnInit {
       'assets/images/gallery/image (6).jpg',
     ];
     
-    this.startAutoSlide();
+    // this.startAutoSlide();  // Start auto slide when the component initializes
   }
 
   ngOnDestroy() {
-    this.stopAutoSlide();
+    // this.stopAutoSlide();  // Clean up interval when component is destroyed
   }
 
   nextSlide() {
     this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.cdRef.detectChanges();  // Manually trigger change detection
   }
 
   previousSlide() {
     this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.cdRef.detectChanges();  // Manually trigger change detection
   }
 
   private startAutoSlide() {
+    // Start the auto slide interval
     this.autoSlideInterval = setInterval(() => {
-      this.nextSlide();
-    }, 5000); // Change slide every 5 seconds
+      this.nextSlide();  // Move to the next slide every 5 seconds
+    }, 5000);
   }
 
   private stopAutoSlide() {
+    // Clear the interval when the component is destroyed
     if (this.autoSlideInterval) {
       clearInterval(this.autoSlideInterval);
     }
