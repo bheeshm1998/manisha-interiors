c269cbe657e52af6aeb3fe35df8d57ad442b56a4
PhysicsIsLove <aa.iitbbs@gmail.com>
2024-12-15 17:32:30 +0530

    added footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  currentYear = new Date().getFullYear();
+  
+  // Company working hours
+  workingHours = [
+    { day: 'Monday - Friday', hours: '9:00 AM - 6:00 PM' },
+    { day: 'Saturday', hours: '10:00 AM - 4:00 PM' },
+    { day: 'Sunday', hours: 'Closed' }
+  ];
 
+  // Social media links
+  socialLinks = [
+    { icon: 'fab fa-facebook-f', url: '#', label: 'Facebook' },
+    { icon: 'fab fa-instagram', url: '#', label: 'Instagram' },
+    { icon: 'fab fa-pinterest', url: '#', label: 'Pinterest' },
+    { icon: 'fab fa-linkedin-in', url: '#', label: 'LinkedIn' }
+  ];
 }
