64974d7391fc5c94d5c76f53a0f404d17a2e78da
PhysicsIsLove <aa.iitbbs@gmail.com>
2024-12-22 01:42:32 +0530

    removed unncessary logs and comments and imports

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BUSINESS_ADDRESS, BUSINESS_EMAIL, BUSINESS_PHONE } from '../../shared/models/constants';
-// import { GoogleMapsModule } from '@angular/google-maps';
+
 
 @Component({
   selector: 'app-contact',
@@ -18,20 +18,7 @@ export class ContactComponent {
   businessPhone: String = BUSINESS_PHONE;
   businessAddress: String = BUSINESS_ADDRESS
   
-  // Google Maps configuration
   display: any;
-  // center: google.maps.LatLngLiteral = {
-  //   lat: 12.9716,
-  //   lng: 77.5946
-  // };
-  // zoom = 15;
-  
-  // Simplified map options
-  // mapOptions: google.maps.MapOptions = {
-  //   disableDefaultUI: true,
-  //   zoomControl: true,
-  //   scrollwheel: false,
-  // };
 
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
