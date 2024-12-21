985fc220af0c863cfbf80c121073449f7b8a4307
PhysicsIsLove <aa.iitbbs@gmail.com>
2024-12-21 12:04:41 +0530

    dummy change to test the deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-// import { GoogleMapsModule } from '@angular/google-maps';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
