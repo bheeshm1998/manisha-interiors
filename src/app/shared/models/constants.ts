9b148a2a602bcae1fa7ee566a1bb50f48f5a97e9
PhysicsIsLove <aa.iitbbs@gmail.com>
2024-12-25 14:42:15 +0530

    added message sending functionality

--- src/app/shared/models/constants.ts
+++ src/app/shared/models/constants.ts
@@ -27,3 +27,8 @@ export const BUSINESS_TAGLINES = [
   ]
   
 export const ABOUT_US_CONTENT = `Manisha Interiors is a full-service interior design firm that specializes in residential projects. Our team of experienced designers and architects work closely with clients to create spaces that are both functional and beautiful. From concept to completion, we are committed to delivering exceptional design solutions that exceed our clients' expectations. Whether you are looking to renovate your home or office, our team will work with you to create a space that reflects your personal style and meets your specific needs. Contact us today to schedule a consultation and learn more about our services.`
+
+
+export const EMAIL_JS_SERVICE_ID = "service_manisha_interior"
+export const EMAIL_JS_TEMPLATE_ID = "template_92y3j0d"
+export const EMAIL_JS_PUBLIC_KEY = "_HQSCHYyW15rXUgdA"
